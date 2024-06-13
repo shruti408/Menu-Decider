298bbf4b7a722b1c5a5ff09bc7f951c7dc2d15ef
shruti408 <127991092+shruti408@users.noreply.github.com>
2024-06-13 16:21:33 +0530

    Update

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,7 +5,7 @@ import UserContext from "./context/usercontext"
 export default function Navbar() {
     const { user, logout } = useContext(UserContext);
 
-    function handleLogout(e) {
+    async function handleLogout(e) {
         e.preventDefault();
         await logout();
          navigate("/");
