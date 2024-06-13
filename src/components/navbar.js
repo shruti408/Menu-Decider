43c26998003de48b22a9376568ec366fd55f34a1
shruti408 <127991092+shruti408@users.noreply.github.com>
2024-06-13 16:10:08 +0530

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,7 +7,9 @@ export default function Navbar() {
 
     function handleLogout(e) {
         e.preventDefault();
-        logout();
+        await logout();
+         navigate("/");
+         window.location.reload();
     }
 
     return (
